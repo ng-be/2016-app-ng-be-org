75b9c571f05bcab4ad6a55974f2d9bbee7627b98
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2016-09-25 21:05:33 +0200

    added remark for firebase details

--- app/app.ts
+++ app/app.ts
@@ -7,17 +7,23 @@ import { ConferenceData } from "./providers/conference-data";
 import { UserData } from "./providers/user-data";
 import { AppContainer } from "./containers/app.container";
 
-ionicBootstrap(AppContainer, [ConferenceData, UserData,
-  FIREBASE_PROVIDERS,
-  defaultFirebase({
-    apiKey: 'xxx',
-    authDomain: 'xxx',
-    databaseURL: 'xxx',
-    storageBucket: 'xxx',
-  }),
-  firebaseAuthConfig({
-    provider: AuthProviders.Password,
-    method: AuthMethods.Password,
-    remember: "default",
-    scope: ["email"]
-  })], {});
+ionicBootstrap(AppContainer,
+  [
+    ConferenceData,
+    UserData,
+    FIREBASE_PROVIDERS,
+    defaultFirebase({
+      apiKey: 'xxx',          // ask core member for details
+      authDomain: 'xxx',      // ask core member for details
+      databaseURL: 'xxx',     // ask core member for details
+      storageBucket: 'xxx',   // ask core member for details
+    }),
+    firebaseAuthConfig({
+      provider: AuthProviders.Password,
+      method: AuthMethods.Password,
+      remember: "default",
+      scope: ["email"]
+    })
+  ],
+  {}
+);
