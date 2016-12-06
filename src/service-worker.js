b4e296a26c32e43c77b9a2c39d367c190e6d5bf8
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2016-12-06 21:41:10 +0100

    (fix) updated serviceworker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,5 @@
 // tick this to make the cache invalidate and update
-const CACHE_VERSION = 1611281920;
+const CACHE_VERSION = 1612062140;
 const CURRENT_CACHES = {
     'read-through': 'read-through-cache-v' + CACHE_VERSION
 };
